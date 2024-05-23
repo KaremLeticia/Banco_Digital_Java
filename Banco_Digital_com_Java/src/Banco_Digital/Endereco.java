2322acd7dfd80ae22720ae0f2045626fc1e3531c
Karem Leticia <karemsantosleticia@gmail.com>
2024-05-22 22:13:23 -0300

    Subindo projeto para o Repositório

---
+++ Banco_Digital_com_Java/src/Banco_Digital/Endereco.java
@@ -0,0 +1,10 @@
+package Banco_Digital;
+
+import lombok.AllArgsConstructor;
+@AllArgsConstructor
+public class Endereco {
+    private String endereco;
+    private String bairro;
+    private String cidade;
+    private String uf;
+}
---
+++ Banco_Digital_com_Java/src/Banco_Digital/TipoDeConta.java
@@ -0,0 +1,15 @@
+package Banco_Digital;
+
+public enum TipoDeConta {
+    CONTA_CORRENTE("Conta Corrente"),
+    CONTA_POUPANÇA("Conta Poupança");
+
+    private String  tipoDeConta;
+    TipoDeConta(String tipoDeConta) {
+        this.tipoDeConta = tipoDeConta;
+    }
+
+    public String getTipoDeConta(){
+        return this.tipoDeConta;
+    }
+}
